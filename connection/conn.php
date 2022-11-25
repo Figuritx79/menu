83a3fc207845a43b583e455c2489cdfa1998bc94
Enrique González <gonherenrique@gmail.com>
2022-11-24 20:02:19 -0600

    ¡Listo para subir a Railway 2 xD

--- connection/conn.php
+++ connection/conn.php
@@ -1,7 +1,8 @@
 <?php
-$server="127.0.0.1:33065";
-$user="root";
-$pass="";
-$db="enrique_gonzalez";
-$conexion=mysqli_connect($server,$user,$pass,$db); 
+$DB_HOST=$_ENV["DB_HOST"];
+$DB_USER=$_ENV["DB_USER"];
+$DB_PASSWORD=$_ENV["DB_PASSWORD"];
+$DB_NAME=$_ENV["DB_NAME"];
+$DB_PORT=$_ENV["DB_PORT"];
+$conexion=mysqli_connect($DB_HOST,$DB_USER,$DB_PASSWORD,$DB_NAME,$DB_PORT); 
 ?>
